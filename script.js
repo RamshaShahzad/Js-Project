cae01a8047cfe4862a750104c36f74fde925fd8e
Ramsha Shahzad <ramshashahzad113@gmail.com>
2023-09-28 21:32:19 +0500

    javascript code added

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+// const key = "1f73a5a2";
+
+// fetch("http://www.omdbapi.com/?s=tt3896198&apikey=1f73a5a2")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
 const clickHandler = () => {
-    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=1f73a5a2")
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((resp) => {
-        console.log("resp: ", resp);
+  const search = document.getElementById("search").value;
+
+  fetch(`http://www.omdbapi.com/?s=${search}&apikey=1f73a5a2`)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data.Search[0].Title);
+      // UL tag id
+      const ulTag = document.getElementById("movieList");
+      data.Search.map(resultMovies);
+      function resultMovies(movie) {
+        const listItem = `
+          <li>
+          <div>
+          <p>Title: ${movie.Title}</p>
+          <p>Year: ${movie.Year}</p>
+          <img src="${movie.Poster}" alt="Image not found">
+          </div>
+          </li>
+          `;
+        ulTag.innerHTML = ulTag.innerHTML + listItem;
+      }
+    });
+};
