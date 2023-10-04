89b88f6aa20c597889b6c17b796878c1bcf3f0de
Ramsha Shahzad <ramshashahzad113@gmail.com>
2023-10-04 17:19:46 +0500

    filter bar is now working

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const clickHandler = () => {
   const search = document.getElementById("search").value;
-
+  if (!search) {
+    return;
+  }
   fetch(`http://www.omdbapi.com/?s=${search}&apikey=1f73a5a2`)
     .then((resp) => {
       return resp.json();
     })
 
     .then((data) => {
+      // console.log(data);
       console.log(data.Search[0].Title);
       // UL tag id
+      let mydata = data.Search;
       const ulTag = document.getElementById("movieList");
-      data.Search.map(resultMovies);
+      ulTag.innerHTML = "";
+      const year = document.getElementById("filter").value;
+      console.log(year);
+      if (year) {
+        mydata = mydata.filter((movie) => movie.Year >= Number(year));
+      }
+      console.log(mydata);
+      mydata.map(resultMovies);
       function resultMovies(movie) {
         const listItem = `
           <li>
